686317e2fa61526cbcdf68bfb25dcb384f5fd36a
SirhcG <planbhide@gmail.com>
2017-02-19 04:33:30 -0800

    Add files via upload

--- gamebase/game.java
+++ gamebase/game.java
@@ -7,9 +7,9 @@ public class game {
 	public static void main(String[] args) {
 		
 		CardMonsters game = new CardMonsters();
-		
+		game.go();
 		TyrantUnleashed game2 = new TyrantUnleashed();
-		game2.go();
+		//game2.go();
 
 	}
 
